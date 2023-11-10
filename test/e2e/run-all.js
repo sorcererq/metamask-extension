784bd5c640e15d5eb7c3731f8ea9d46e017252ab
Howard Braham <howrad@gmail.com>
2023-11-09 18:17:51 -0800

    testing errors.spec.js

--- test/e2e/run-all.js
+++ test/e2e/run-all.js
@@ -198,6 +198,8 @@ async function main() {
     myTestList = testPaths;
   }
 
+  myTestList = ['/home/circleci/project/test/e2e/tests/errors.spec.js'];
+
   console.log('My test list:', myTestList);
 
   // spawn `run-e2e-test.js` for each test in myTestList
